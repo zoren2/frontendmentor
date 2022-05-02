c8ef4a6b0b501eb2bb536d5f15e83e8fa5b09ae1
Jack Tsang <cic_tian2@yahoo.com>
2022-05-01 21:28:07 -0700

    Added mobile navbar to invoice-app

--- invoice-app/src/app/app.module.ts
+++ invoice-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
