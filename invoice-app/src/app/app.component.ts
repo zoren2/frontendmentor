3d186b4a9a50d2819a7b1d2c9f6a3e92d6a18b83
Jack Tsang <cic_tian2@yahoo.com>
2022-05-03 15:56:51 -0700

    Added View Invoice component mobile design to invoice-app

--- invoice-app/src/app/app.component.ts
+++ invoice-app/src/app/app.component.ts
@@ -8,8 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'invoice-app';
   hasInvoices: boolean = true;
+  editingInvoice: boolean = false;
 
   toggleHasInvoice(): void {
     this.hasInvoices = !this.hasInvoices;
+  }
+
+  toggleEdit(): void {
+    this.editingInvoice = !this.editingInvoice;
+  }
+
+  backToMain(event: string) {
+    if(event == 'back-to-main') {
+      this.hasInvoices = true;
+      this.editingInvoice = false;
+    }
   }
 }
--- invoice-app/src/app/app.module.ts
+++ invoice-app/src/app/app.module.ts
@@ -6,15 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InvoiceComponent } from './invoice/invoice.component';
 
+/* Material Imports */
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ViewInvoiceComponent } from './view-invoice/view-invoice.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    InvoiceComponent
+    InvoiceComponent,
+    ViewInvoiceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
