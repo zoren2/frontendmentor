a90f586fc93369a5f52bb05cd2ded85aa047a47b
Jack Tsang <cic_tian2@yahoo.com>
2022-05-03 10:19:13 -0700

    Added mobile invoice card design to invoice-app

--- invoice-app/src/app/app.component.ts
+++ invoice-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'invoice-app';
+  hasInvoices: boolean = true;
+
+  toggleHasInvoice(): void {
+    this.hasInvoices = !this.hasInvoices;
+  }
 }
