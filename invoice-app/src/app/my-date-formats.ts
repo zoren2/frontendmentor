e421849eb75e0dd646c55585abe3c00421cc3e4a
Jack Tsang <cic_tian2@yahoo.com>
2022-05-29 18:23:02 -0700

    Added time format for edit-component calendar for invoice-app

---
+++ invoice-app/src/app/my-date-formats.ts
@@ -0,0 +1,13 @@
+export const MY_DATE_FORMATS = {
+    parse: {
+        // Parsed from input
+        dateInput: 'MM-DD-YYYY',
+    },
+    display: {
+        // Desired format
+        dateInput: 'MMM-DD-YYYY',
+        monthYearLabel: 'MMMM YYYY',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'MMMM YYYY'
+    },
+}; 
