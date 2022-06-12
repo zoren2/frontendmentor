3de25affce5e29b1e92ef00d144e44ad235d4881
Jack Tsang <cic_tian2@yahoo.com>
2022-06-12 15:03:15 -0700

    Added Desktop Design to dine-app

--- dine-app/src/app/app-routing.module.ts
+++ dine-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'reservation', component: ReservationComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
