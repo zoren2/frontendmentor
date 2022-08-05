7c3c6e22d7a91042b660c88b0f6fb29ed16d90c1
Jack Tsang <cic_tian2@yahoo.com>
2022-08-05 16:54:13 -0700

    Added edit-feedback component to product-feedback

---
+++ product-feedback/src/app/edit-feedback/edit-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-feedback',
+  templateUrl: './edit-feedback.component.html',
+  styleUrls: ['./edit-feedback.component.scss']
+})
+export class EditFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
