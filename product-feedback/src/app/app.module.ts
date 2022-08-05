4fc034580dc08d1e8a9b68b767d11970a76262cd
Jack Tsang <cic_tian2@yahoo.com>
2022-08-05 14:00:44 -0700

    Added animations to product-feedback

--- product-feedback/src/app/app.module.ts
+++ product-feedback/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavAddFeedbackComponent } from './nav-add-feedback/nav-add-feedback.component';
+import { FeedbackCardComponent } from './feedback-card/feedback-card.component'
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, RouterModule.forRoot(routes), BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent, NavMenuComponent, NavAddFeedbackComponent, FeedbackCardComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
