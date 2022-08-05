4c3b163a4bc2fb69e600b33ff6507723e3c6db5d
Jack Tsang <cic_tian2@yahoo.com>
2022-08-05 14:00:17 -0700

    Added feedback-card component to product-feedback

---
+++ product-feedback/src/app/feedback-card/feedback-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-card',
+  templateUrl: './feedback-card.component.html',
+  styleUrls: ['./feedback-card.component.scss']
+})
+export class FeedbackCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
