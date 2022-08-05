3cbb1f73ed01d4b58b39b1018221d43c87bbfd32
Jack Tsang <cic_tian2@yahoo.com>
2022-08-05 16:55:48 -0700

    Added new-feedback component to product-feedback

---
+++ product-feedback/src/app/new-feedback/new-feedback.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewFeedbackComponent } from './new-feedback.component';
+
+describe('NewFeedbackComponent', () => {
+  let component: NewFeedbackComponent;
+  let fixture: ComponentFixture<NewFeedbackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewFeedbackComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewFeedbackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
