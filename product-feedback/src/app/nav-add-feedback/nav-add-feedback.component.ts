02fd1785a730be329391711d7f79a3d59ea932e0
Jack Tsang <cic_tian2@yahoo.com>
2022-08-09 17:05:13 -0700

    Added reactive forms for nav-add-feedback component of product-feedback

--- product-feedback/src/app/nav-add-feedback/nav-add-feedback.component.ts
+++ product-feedback/src/app/nav-add-feedback/nav-add-feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-add-feedback',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavAddFeedbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onClickNewFeedback(event: Event) {
+    event.stopPropagation();
+    this.router.navigateByUrl('/addFeedback');
   }
 
 }
