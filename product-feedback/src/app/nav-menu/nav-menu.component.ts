2c3e51f82bd1d86740ff9c4914cc48b09571c2da
Jack Tsang <cic_tian2@yahoo.com>
2022-08-11 13:36:27 -0700

    Added roadmap-nav modal to product-feedback

--- product-feedback/src/app/nav-menu/nav-menu.component.ts
+++ product-feedback/src/app/nav-menu/nav-menu.component.ts
@@ -1,10 +1,12 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';;
+import { Component, OnInit, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   animations: [
     trigger('hamburgerX', [
       /*
@@ -52,15 +54,42 @@ import { Component, OnInit } from '@angular/core';
 
 export class NavMenuComponent implements OnInit {
 
+  /* Screen Resolutions */
+  public screenWidth!: number;
+  public screenHeight!: number;
   isHamburger = true;
 
-  constructor() { }
+  dialogRef!: MatDialogRef<any>;
+
+
+  constructor(private dialog: MatDialog) {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
 
   ngOnInit(): void {
   }
 
-  onClick() {
+  onClick(templateRef: TemplateRef<any>) {
     console.log("clicked");
     this.isHamburger = !this.isHamburger;
+    this.openFormDialog(templateRef);
   }
+
+  private openFormDialog(templateRef: TemplateRef<any>) {
+    const modalHeight = (this.screenHeight - 72) + 'px';
+    this.dialogRef = this.dialog.open(templateRef, {
+      height: modalHeight,
+      width: '72.27vw',
+      position: {
+        right: '0',
+        top: '72px'
+      }
+    });
+    this.dialogRef.afterClosed().subscribe(() => {
+      this.isHamburger = !this.isHamburger
+    });
+  }
+
+
 }
